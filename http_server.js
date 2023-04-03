4a712ca6f58236a2c58d2a1200048e1e81951be6
Kate Burnett <119454443+KateBurnett@users.noreply.github.com>
2023-04-03 19:21:56 -0400

    remove cors def

--- http_server.js
+++ http_server.js
@@ -4,7 +4,7 @@ const low = require('lowdb');
 const fs = require('lowdb/adapters/FileSync');
 const adapter = new fs('db.json');
 const db = low(adapter);
-const cors = require('cors');
+//const cors = require('cors');
 const { faker } = require('@faker-js/faker');
 
 // data parser - used to parse post data
