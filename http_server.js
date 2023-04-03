9abe31e947da342c233b5731281dca23878c7743
Kate Burnett <119454443+KateBurnett@users.noreply.github.com>
2023-04-03 19:27:51 -0400

    Update http_server.js

--- http_server.js
+++ http_server.js
@@ -1,10 +1,10 @@
+banana
 const express = require('express');
 const app = express();
 const low = require('lowdb');
 const fs = require('lowdb/adapters/FileSync');
 const adapter = new fs('db.json');
 const db = low(adapter);
-banana
 //const cors = require('cors');
 const { faker } = require('@faker-js/faker');
 
